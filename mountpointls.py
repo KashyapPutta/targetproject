2b193f0f73a4d83022dd73a5ca99315117246d8b
Kashyap Babu Chowdary Putta <kbcputta.1994@gmail.com>
2021-12-06 01:32:34 -0700

    improved the string formatting of shell code

--- mountpointls.py
+++ mountpointls.py
@@ -5,18 +5,18 @@ import json
 
 def monitorFileSystem(mountInput):
     fileSystemData = subprocess.check_output(
-        "cd %s; cat /proc/mounts"
-        "| grep -w %s"
+        "MOUNTPOINT=%s && cd $MOUNTPOINT && cat /proc/mounts"
+        "| grep $MOUNTPOINT"
         "| awk '{print $2}'"
         "| xargs ls"
         "| awk '{print substr($0, index($0,$1))}'"
         "| xargs -d '\n' du"
-        "| awk -v OFS=\"#\" '{print $1, substr($0, index($0,$2))}'" % (mountInput, mountInput), shell=True).decode("utf-8")
+        "| awk -v OFS=\"#\" '{print $1, substr($0, index($0,$2))}'" % (mountInput), stderr=subprocess.STDOUT, shell=True).decode("utf-8")
     rawFileList = fileSystemData.split("\n")
     fileSystemDict = {}
     fileList = []
     for file in rawFileList:
-        if file != '/':
+        if file != '':
             splitlist = file.split('#',1)
             if len(splitlist) >= 2:
                 individualFile = {}
@@ -25,15 +25,17 @@ def monitorFileSystem(mountInput):
                 fileSize = splitlist[0]
                 individualFile[fileAbsolutePath] = fileSize
                 fileList.append(individualFile)
+            else:
+                print("Invalid file contents. Less than 2 elements in splitList")
     fileSystemDict["files"] = fileList
     return fileSystemDict
 
-
 def main():
-    mountInput = sys.argv[1]
-    # mountInput = "/Users/p2931556/PycharmProjects/targetproject/opt/twc/nationalnavigation/components/navigation/conf/symphoni"
-    fileSystemDict = monitorFileSystem(mountInput)
-    print(json.dumps(fileSystemDict, indent=1))
-
+    try:
+        mountInput = sys.argv[1]
+        fileSystemDict = monitorFileSystem(mountInput)
+        print(json.dumps(fileSystemDict, indent=1).replace("\\",""))
+    except subprocess.CalledProcessError as e:
+        print("Exception occured: {}".format(e.output))
 
 main()
